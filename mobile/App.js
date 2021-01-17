c9db303bbfb63de2703a55b6a32b286e3bbc17b1
Marcus Chong <marcus.jn.chong@gmail.com>
2021-01-17 00:54:17 -0500

    added ui wireframe

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, Component } from 'react';
-import { View, Text, AsyncStorageStatic } from 'react-native';
+import { Picker, StyleSheet,View, Text, AsyncStorageStatic, SafeAreaView, ScrollView, TextInput } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+    },
+    scrollView: {
+      marginHorizontal: 20,
+    },
+    text: {
+      fontSize: 20,
+    },
+  });
 
 export default function App() {
 
@@ -52,9 +65,168 @@ export default function App() {
       justifyContent: "center",
       alignItems: "center",
     }}>
-      <Text>
-        Your Token is: 
-      </Text>
+
+      <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View>
+        <Text>
+            Your Token is: (token)
+        </Text>
+        <ScrollView style={{margin:10, maxHeight: 400 }} nestedScrollEnabled={true} >
+
+        <Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text>
+
+            <Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text><Text h3 style={{ fontWeight: 'bold', fontSize: 16}}>
+                person name
+            </Text>
+
+            <Text>
+                text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here text goes here 
+            </Text>
+
+
+        </ScrollView>
+        </View>
+        <View>
+            <Text> Comma-separated list of keywords: </Text>
+            <View style={{backgroundColor: '#CDCDCD'}}>
+                <TextInput> </TextInput>
+            </View>
+        </View>
+
+        <View>
+
+        <Text> {"\n \n"}Send a response: </Text>
+
+      <Picker
+        style={{ height: 50, width: 150 }}
+        // onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+          {
+              ['yes', 'no', 'my wifi isnt working', 'my mic isnt working', 'give me a second'].map((val) => {
+                    var k = Math.random();
+                  return <Picker.Item key = {k} label={val} value={val} />
+              })
+          }
+        {/* <Picker.Item label="yes" value="yes" /> */}
+        {/* <Picker.Item label="no" value="no" /> */}
+
+      </Picker>
+        </View>
+      </ScrollView>
+
+    </SafeAreaView>
+    
+
+
     </View>
   );
 }
